e553a319a7bf1d030cb43c807e48faccb558d878
shon-cyber <dharshongowri925@gmail.com>
2025-12-29 21:36:56 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,7 @@
+class Add{
+
+public static void main(String[] args){
+int sum=(6+7);
+System.out.println(sum);
+}
+}
---
+++ Datatype.java
@@ -0,0 +1,8 @@
+class Datatype{
+public static void main(String[] args){
+int a=9;
+float b=11.2f;
+float sum=(a+b);
+System.out.println(sum);
+}
+}
